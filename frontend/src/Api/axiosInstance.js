31d0a4691f1648e65b080c8df453a36bf2e609ee
Niloy Gope <ngcomputer1517@gmail.com>
2026-01-21 15:27:46 +0600

    Update axios baseURL to include '/api' path

--- frontend/src/Api/axiosInstance.js
+++ frontend/src/Api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://foodify-backend-8a60.onrender.com",
+    baseURL: "https://foodify-backend-8a60.onrender.com/api",
     withCredentials: true,
     headers: {'Content-Type': 'Application/json'}
 })
